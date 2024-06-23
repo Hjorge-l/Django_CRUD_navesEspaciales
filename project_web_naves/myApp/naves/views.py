a572658536d7cd059c423e40e0ab74cc27b6c47e
Hjorge-l <haroljorgelopez@gmail.com>
2024-06-23 02:50:15 +0200

    Se ha implementado la opción de ver todas las naves con todos sus atributas en la web

--- project_web_naves/myApp/naves/views.py
+++ project_web_naves/myApp/naves/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from django.shortcuts import render, redirect
 from django.conf import settings
 from myApp.serializer import NaveSerializer
+from myApp.models import naveModel
 
 
 @api_view(['GET', 'POST'])
@@ -14,9 +15,18 @@ def insert_data(request):
         serializer = NaveSerializer(data=datos_nave)
         if serializer.is_valid():
             serializer.save()   # Insertamos en la base de datos
-            print("Writing complete")
             result = 'Insertado correctamente en la base de datos'
             return render(request, 'naves/insert.html',
                           context={'result': result},
                           status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def show_all_data(request):
+    if request.method == 'GET':
+        naves_query = naveModel.objects.all()
+        serializer = NaveSerializer(naves_query, many=True)
+        datos_nave = serializer.data
+        return render(request, "naves/show_all.html",
+                      context={"datos_nave": datos_nave})
--- project_web_naves/myApp/urls.py
+++ project_web_naves/myApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import re_path, include
 from myApp import views
-from myApp.naves.views import insert_data
+from myApp.naves.views import insert_data, show_all_data
 
 urlpatterns = [
     re_path(r"^inicio/", views.inicio, name='inicio'),
     re_path('accounts/', include('django.contrib.auth.urls')),
     re_path(r"^register/", views.register, name='register'),
     re_path(r"^insert_data/", insert_data, name='insert_data'),
+    re_path(r"^show_all_data/", show_all_data, name='show_all_data'),
 ]
